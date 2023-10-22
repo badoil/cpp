aaaf285877b290952e33fa6e95084dd4b1791fe6
oneal.oh <oneal.oh@kakaomobility.com>
2023-10-22 14:36:03 +0900

    fix: 8-m.cpp

--- algorithm/week8(graph)/8-m copy.cpp
+++ algorithm/week8(graph)/8-m copy.cpp
@@ -21,77 +21,56 @@ using namespace std;
 #define INF 987654321
 
 int n, m, t, d, a[30][30], dp[3000][3000], ret;
-const int dy[] = {-1, 0, 1, 0};
-const int dx[] = {0, 1, 0, -1};
-
 vector<int> v;
-string s;
+int dy[4] = {-1, 0, 1, 0};
+int dx[4] = {0, 1, 0, -1};
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    ios_base::sync_with_stdio(false)
+    cin.tie(NULL)
+    cout.tie(NULL)
 
     cin >> n >> m >> t >> d;
     for (int i=0; i<n; i++) {
-        cin >> s;
+        string s;
         for (int j=0; j<m; j++) {
-            if (s[j] >= 'a') a[i][j] = s[j] - 'a' + 26;
-            else a[i][j] = s[j] - 'A';
+            if (s[j] - 'a' >= 0 ) a[i][j] = s[j] - 'a' + 26;
+            else a[i][j] = 1
         }
     }
 
-    for (int i=0; i<n; i++) {
+     for (int i=0; i<n; i++) {
         for (int j=0; j<m; j++) {
-            v.push_back(i*100+j);       // 플로이드-워셜 알고리즘 순회하기위해 2차원좌표를 1차원좌표로 벡터 인덱스 만들어줌
+            v.push_back(i*100+j);
         }
-    }
+     }
 
-    ret = a[0][0];
+    ret = a[0][0]
     fill(&dp[0][0], &dp[0][0] + 3000*3000, INF);
-    
-    // dp[i][j] 디피를 생성, 이때 디피는 2차원좌표 -> 2차원좌표로 표현하는데, 2차원좌표를 i*100+j 로 1차원형식으로 표현
-    // dp는 각 지점들로 이동할때마다의 시간을 저장
     for (int i=0; i<n; i++) {
         for (int j=0; j<m; j++) {
             for (int k=0; k<4; k++) {
-                int ny = i + dy[k];
-                int nx = j + dx[k];
-                int diff = abs(a[i][j]-a[ny][nx]);
-                if (ny>=n || nx>=m || ny<0 || nx<0) continue;
-                if (diff <= t) {
-                    if (a[i][j]<a[ny][nx]) dp[i*100+j][ny*100+nx] = diff * diff;        // 이렇게 하려고 2차원을 1차원으로 바꾼거임
-                    else dp[i*100+j][ny*100+nx] = 1;
-                }
+                ny = a[i][j] + dy[k];
+                nx = a[i][j] + dx[k];
+                if (ny<0 || nx<0 || ny>=n || nx>=m) continue;
+                int diff = abs(a[y][x]-a[ny][nx]);
+                if (a[ny][nx] > a[y][x]) dp[ny][nx] = diff*diff;
+                else dp[ny][nx] = 1;
+            }
+        }
+    }
+
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            for (int k=0; k<4; k++) {
+                
 
             }
         }
     }
 
-    // 플로이드-워셜 조짐
-    // 최단시간이 최단경로이므로
-    // 주어진 시간동안 가장 높이 가야하므로
-    for (int k: v) {
-        for (int i: v) {
-            for (int j: v) {
-                dp[i][j] = min(dp[i][j], dp[i][k]+dp[k][j]);
-            }
-        }
-    }
-
-    for(int i: v){
-        if (d >= dp[0][i] + dp[i][0]) {         // 호텔 0에서 산 i + 산 i에서 호텔 0으로 왕복한 거리, 즉 그 시간이 어두워지기 전이면
-            ret = max(ret, a[i/100][i%100]);    // 모든 산까지중에 최고 높이 구하기
-        }
-    }
-
-    cout << ret << "\n";
-    return 0;
 }
 
 
 // 플로이드-워셜 문제
 // 2차원좌표를 1차원좌표로 바꿔 표현하기가 핵심
-
-// dp 맵에서는 시간을 저장
-// a 맵은 높이를 저장
